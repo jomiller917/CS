4521472d832338452951608142a8aeb087efcb19
jomiller <jomiller@my.bridgeport.edu>
2025-09-24 14:22:57 -0400

    Initial changes

--- src/TextExample.java
+++ src/TextExample.java
@@ -1,6 +1,7 @@
 public class TextExample {
     public static void main(String[] args) {
         System.out.println("Testing Git...");
+        System.out.println("Testing GitHub API...");
 
     }
 }
